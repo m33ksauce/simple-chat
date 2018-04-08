aff544a7822becf2518ba8610561c0704b119363
Chris Meek <chris@localhost.localdomain>
2018-04-08 19:28:21 -0400

    Adding initial commit and messages api

---
+++ routes/messages.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+Message = function(user, messageBody) {
+    this.user = user;
+    this.messageBody = messageBody;
+}
+
+MessageList = function() {
+    var messages = new Array();
+
+    addMessage = function(msg) {
+        messages.push(msg);
+    };
+
+    getNewMessages = function(lastOrder) {
+        var newMessages = new Array();
+        for (i = lastOrder; i < messages.length; i++) {
+            newMessages.push(messages[i]);
+        }
+        return {
+            "countNewMessages": newMessages.length,
+            "newMessages": newMessages,
+            "newOrder": messages.length
+        };
+    };
+
+    getAllMessages = function() {
+        return messages;
+    }
+
+    return {
+        addMessage,
+        getNewMessages, 
+        getAllMessages
+    }
+}
+
+messages = MessageList();
+
+router.get('/', function(req, res, next) {
+  res.send(JSON.stringify(messages.getAllMessages()));
+});
+
+router.get('/getFromOrder/:order', function(req, res, next) {
+    res.send(JSON.stringify(messages.getNewMessages(req.params["order"])));
+});
+
+router.post('/newMessage', function(req, res) {
+    messages.addMessage(new Message(req.body.msg.user, req.body.msg.messageBody))
+    res.sendStatus(200);
+});
+
+module.exports = router;
